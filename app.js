9915f4b2aa6d66772a70aaf51df183bf75613f9e
firdavs0707 <firdavs0.4.2.0.0.7>
2022-07-31 00:39:02 +0500

    add codes

--- app.js
+++ app.js
@@ -1,33 +1,3 @@
-// //                    VARIABLES
-
-// var input = document.querySelector('.input1');
-// var btn = document.querySelector('.btn');
-// var result = document.querySelector('.result');
-// var form = document.querySelector('.form');
-
-// //                   MAIN LOGIC
-
-// form.addEventListener('submit', function(evt){
-//   evt.preventDefault();
-
-//   if (isNaN(input.value)) {
-//     result.textContent = 'Son kiriting!';
-//   }
-//   else if (input.value > 60) {
-//     result.textContent = "Sizga chipta bepul!";
-//   }
-//   else if (input.value > 20) {
-//     result.textContent = "Siz 2000 so'm to'laysiz !";
-//   }
-//   else if (input.value < 7) {
-//     result.textContent = 'Sizga chipta bepul!';
-//   }
-//   else if (input.value > 7) {
-//     result.textContent = "Siz 1400 so'm to'laysiz !";
-//   }
-// })
-
-
 var form = document.querySelector('.form');
 var input = document.querySelector('.input1');
 var btn = document.querySelector('.btn');
@@ -46,6 +16,8 @@ form.addEventListener('submit', function (evt){
     result.textContent = 'Sizga chipta narxi 2.000 sum';
   } else if (input.value > 6) {
     result.textContent = 'Sizga chipta narxi 1.400 sum';
+  } else if (input.value == 0 ) {
+    result.textContent = 'Yoshingizni kiriting!';
   } else if (input.value < 0 ) {
     result.textContent = 'Xaqiqiy yoshingizni kiriting!';
   } else if (input.value < 7 ) {
