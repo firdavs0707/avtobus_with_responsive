9e66a67673d3cb55faa6fad02492a0be34dc7b5a
firdavs0707 <firdavs0.4.2.0.0.7>
2022-07-31 11:42:07 +0500

    add codes

--- app.js
+++ app.js
@@ -3,24 +3,49 @@ var input = document.querySelector('.input1');
 var btn = document.querySelector('.btn');
 var result = document.querySelector('.result');
 
+result.textContent = '👆Yoshingizni kiriting!';
+
 form.addEventListener('submit', function (evt){
   evt.preventDefault();
 
   if (isNaN(input.value)) {
     result.textContent = 'Son kiriting!';
-  } else if (input.value > 130) {
-    result.textContent = 'Xaqiqiy yoshingizni kiriting! ';
-  } else if (input.value > 60) {
-    result.textContent = 'Sizga chipta bepul!';
-  } else if (input.value > 20) {
-    result.textContent = 'Sizga chipta narxi 2.000 sum';
-  } else if (input.value > 6) {
-    result.textContent = 'Sizga chipta narxi 1.400 sum';
-  } else if (input.value == 0 ) {
-    result.textContent = 'Yoshingizni kiriting!';
-  } else if (input.value < 0 ) {
-    result.textContent = 'Xaqiqiy yoshingizni kiriting!';
-  } else if (input.value < 7 ) {
-    result.textContent = 'Sizga chipta bepul!';
+  } else {
+    if (input.value > 130) {
+      result.textContent = 'Xaqiqiy yoshingizni kiriting! ';
+    } else {
+      if (input.value > 60) {
+        result.textContent = 'Sizga chipta bepul!';
+      } else {
+        if (input.value > 20) {
+          result.textContent = 'Sizga chipta narxi 2.000 sum';
+        } else {
+          if (input.value > 6) {
+            result.textContent = 'Sizga chipta narxi 1.400 sum';
+          } else {
+            if (input.value == 0 ) {
+              result.textContent = '👆Yoshingizni kiriting!';
+            } else {
+              if (input.value < 0 ) {
+                result.textContent = 'Xaqiqiy yoshingizni kiriting!';
+              } else {
+                if (input.value < 7 ) {
+                  result.textContent = 'Sizga chipta bepul!';
+                } else {
+                  result.textContent = 'error';
+                }
+              }
+            }
+          }
+        }
+      }
+    }
   } 
 })
+
+
+
+
+
+
+
